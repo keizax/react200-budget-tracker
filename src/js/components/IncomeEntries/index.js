bc6bd0f568d9477f3ada000f15370a6656d4ab7f
JGadi <johnpgadi@gmail.com>
2019-06-25 16:06:47 -0700

    initial commit

---
+++ src/js/components/IncomeEntries/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import IncomeEntries from './IncomeEntries';
+
+export default IncomeEntries;
---
+++ src/js/components/Summary/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Summary from './Summary';
+
+export default Summary;
